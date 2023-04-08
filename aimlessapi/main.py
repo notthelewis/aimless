7634076762ccfc30a1de52e364b6f85807afa3a2
Aimlessfish <96752196+Aimlessfish@users.noreply.github.com>
2023-04-08 15:39:12 +0100

    rest api inbound
    Created /templates/
    > 200.html, 400.html
    
    In main.py
    > added route for endpoint /needs-auth
    - returns 401.html
    > added route for endpoint /wide-open
    - returns 200.html

---
+++ aimlessapi/main.py
@@ -0,0 +1,18 @@
+from flask import Flask, request, send_from_directory, render_template
+import flask_restful
+import flask_sqlalchemy
+import flask_jwt_extended
+import flask_cors
+
+app = Flask(__name__)
+
+@app.route('/needs-auth', methods=["GET"])
+def auth():
+	return render_template('401.html')
+
+@app.route('/wide-open', methods=["GET"])
+def open():
+	return render_template('200.html')
+
+if __name__ == '__main__':
+	app.run(host='0.0.0.0', port='42069', debug=True)
