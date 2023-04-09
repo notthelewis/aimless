a441f78aaa2919d69dddec62eb68dd3baa182876
Lewis Farnworth <lewfar99@gmail.com>
2023-04-09 01:15:10 +0100

    Removed redundant route files

--- aimless/api/needs_auth.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask, request, render_template
-
-app = Flask(__name__)
-
-
-class needs_auth:
-    @app.route('/needs-auth', methods=["GET"])
-    def auth():
-        return render_template('401.html')
--- aimless/api/wide_open.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask, request, render_template
-
-app = Flask(__name__)
-
-
-class wide_open:
-    @app.route('/wide-open', methods=["GET"])
-    def open():
-        return render_template('200.html')
