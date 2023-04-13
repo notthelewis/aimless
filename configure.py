b8fbd33426fc47ca2b0d9c8999c77eaa5a3cc668
Lewis Farnworth <lewfar99@gmail.com>
2023-04-13 23:06:33 +0100

    Containers are now built and brought up. The script is effectively a complete rewrite of the Shell script. Now to test on Windows :)

--- configure.py
+++ configure.py
@@ -1,13 +1,19 @@
 #!/usr/bin/env python3
+
 import os
+from shutil import which
 
 cwd = os.getcwd()
 
 
 def command_exists(name):
     """Check whether `name` is in the PATH"""
-    from shutil import which
     return which(name) is not None
+
+
+def is_valid_csv_entry(L, Omitted):
+    """Ensures L is not blank and is not `Omitted`"""
+    return L != Omitted and L not in ['\n', '\r\n'] and L != ''
 
 
 def check_dependencies():
@@ -19,9 +25,7 @@ def check_dependencies():
         for d in deps:
             d = d.strip()
 
-            # Skip blank line and header
-            valid_entry = d != "dependency_name" and d not in [
-                    '\n', '\r\n'] and d != ''
+            valid_entry = is_valid_csv_entry(d, "dependency_name")
 
             if not valid_entry:
                 continue
@@ -46,9 +50,49 @@ def build_db_schema():
     os.system(f'dbml2sql {cwd}/aimless/db/db-schema.dbml')
 
 
+def build_container(container_name):
+    """Builds Docker container from Dockefile in ./aimless/`container_name`"""
+
+    path = f"{cwd}/{container_name}"
+
+    tag = path.split('/')[-1]
+    tag = f"aimless-{tag}"
+
+    if not os.path.isdir(path):
+        print(f"ERROR: {path} does not exist when trying to build: {tag}")
+        exit(1)
+
+    print(f"Building service: {tag}")
+
+    try:
+        if os.system(f"docker build -t {tag} {path}") != 0:
+            raise Exception
+    except Exception:
+        print(f"Error occured whilst building: {tag}")
+        exit(1)
+
+
+def build_all_containers():
+    """Builds all containers found in `./service-names.csv`"""
+    with open(f"{cwd}/service-names.csv") as services:
+        for C in services:
+            C = C.strip()
+
+            valid_service = is_valid_csv_entry(C, "service_name")
+
+            # Skip blank lines and csv header
+            if not valid_service:
+                continue
+
+            build_container(C)
+
+
 ########################
 # Start of main script #
 ########################
 
 check_dependencies()
 build_db_schema()
+build_all_containers()
+
+os.system('docker compose up -d')
