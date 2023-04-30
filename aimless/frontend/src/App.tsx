dd4063f47fbca40ecbde4905362de7b7da9ac8d1
Lewis Farnworth <lewfar99@gmail.com>
2023-04-30 23:29:12 +0100

    Removed default generated react app, replaced it with skeleton of aimless v0

--- aimless/frontend/src/App.tsx
+++ aimless/frontend/src/App.tsx
@@ -1,19 +1,41 @@
 import React from "react";
-import logo from "./logo.svg";
+
 import "./App.css";
+
+function Content({ children }: { children: React.ReactNode }) {
+    return <p className="content">{children}</p>;
+}
+
+function HeaderMenu({ children }: { children: React.ReactNode }) {
+    return (
+        <div className="headerMenu">
+            <ul>
+                <li></li>
+                <li></li>
+            </ul>
+        </div>
+    );
+}
 
 function App() {
     return (
         <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>
-                    Edit <code>src/App.tsx</code> and save to reload.
-                </p>
-                <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-                    Learn React
-                </a>
+            <header className="header bg_purple fg_purple">
+                <h1>Aimless</h1>
             </header>
+            {/*
+             * Insert menu buttons here (home, explore, sign-in, start-stream, settings)
+             * Menu buttons should not be visible on load, but when scroling down they
+             * should appear from the bottom of the screen, shrinking (alongside the
+             * header image) so that they fill about 20% of the horizontal screen space, as scolling continues.
+             * This then becomes a 'sticky' header, which only expands when the user scrolls right back to the top.
+             * Though, individual menu element will change colour and show tool tips upon scrolling.
+             */}
+            <Content>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur eaque autem impedit praesentium
+                officia exercitationem quod asperiores eos nam magnam quidem dolorum, fugiat dignissimos accusamus
+                inventore excepturi est fuga fugit.
+            </Content>
         </div>
     );
 }
---
+++ aimless/frontend/src/components/HeaderMenu/HeaderMenu.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+import "./HeaderMenu.css";
+
+export default function HeaderMenu() {
+    return (
+        <div className="headerMenu">
+            <ul>
+                <li></li>
+                <li></li>
+            </ul>
+        </div>
+    );
+}
