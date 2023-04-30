fbf9eec6b558f705a25237ba2540f09814f7a9b1
Lewis Farnworth <lewfar99@gmail.com>
2023-04-30 21:32:17 +0100

    Removed old files

--- aimless/rtmp/create_new/src/errors.ts
+++
@@ -1,6 +0,0 @@
-export class ParamLengthError extends Error {
-    constructor(len: number, requiredLen: number) {
-        const string = `Required: ${requiredLen} but got: ${len}`;
-        super(string); 
-    }
-}
