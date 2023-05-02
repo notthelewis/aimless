10e8c1c720eb39ce1605833280a096a6d9d96411
Lewis Farnworth <lewfar99@gmail.com>
2023-05-02 10:52:15 +0100

    Moved animation to global style

--- aimless/frontend/src/components/Header/Header.tsx
+++ aimless/frontend/src/components/Header/Header.tsx
@@ -2,7 +2,7 @@ import "./Header.css";
 
 export default function Header() {
     return (
-        <header className="header">
+        <header className="header shadow_dance">
             <h1>Aimless</h1>
         </header>
     );
--- aimless/frontend/src/components/HeaderMenu/HeaderMenu.tsx
+++ aimless/frontend/src/components/HeaderMenu/HeaderMenu.tsx
@@ -4,7 +4,7 @@ import "./HeaderMenu.css";
 
 export default function HeaderMenu() {
     return (
-        <div className="headerMenu">
+        <div className="headerMenu shadow_dance">
             <ul>
                 <li>Home</li>
                 <li>Explore</li>
